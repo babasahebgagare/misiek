1db023f2da91de07e7d5fbf77598f1c31d374364
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2008-09-23 19:31:09 +0000

    bugfix build add left panel

--- trunk/MCVPlugin/src/main/Main.java
+++ trunk/MCVPlugin/src/main/Main.java
@@ -1,16 +1,11 @@
 package main;
 
-import cytoscape.Cytoscape;
 import cytoscape.plugin.CytoscapePlugin;
-import javax.swing.JOptionPane;
 
 public class Main extends CytoscapePlugin {
 
     public Main() {
-        String message = "Hello World!";
-        System.out.println(message);
-        // use the CytoscapeDesktop as parent for a Swing dialog
-        JOptionPane.showMessageDialog(Cytoscape.getDesktop(), message);
+        PluginInitializator.initAll();
     }
 }
 
