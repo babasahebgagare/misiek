62d885440373abcfc48595b37fd82f18bdf973b0
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2008-09-25 01:10:25 +0000

    dodanie prostego stylu

--- MCVPlugin/src/converter/InteractionsConverter.java
+++ MCVPlugin/src/converter/InteractionsConverter.java
@@ -3,6 +3,7 @@ package converter;
 import cytoscape.CyEdge;
 import cytoscape.CyNetwork;
 import cytoscape.Cytoscape;
+import cytoscape.data.Semantics;
 import java.util.Collection;
 import structs.Interaction;
 
@@ -11,9 +12,9 @@ public class InteractionsConverter {
     static void convertNetworkInteractions(CyNetwork cyNetwork, Collection<Interaction> interactions) {
         for (Interaction interaction : interactions) {
 
-            CyEdge edge = Cytoscape.getCyEdge(interaction.getSourceID(), interaction.getID(), interaction.getTargetID(), "TEST");
+            CyEdge edge = Cytoscape.getCyEdge(interaction.getSourceID(), interaction.getID(), interaction.getTargetID(), Semantics.INTERACTION);
+            edge.setIdentifier(interaction.getID());
             cyNetwork.addEdge(edge.getRootGraphIndex());
-
         }
     }
 }
