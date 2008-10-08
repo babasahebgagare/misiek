e4ca3adc041dd3f504a20a3028575cc679095b3a
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2008-10-08 23:13:51 +0000

    Rozmieszczanie wierzcholkow po okregu wewnatrz duzego wierzcholka

--- trunk/MCVPlugin/src/visual/layout/Layouter.java
+++ trunk/MCVPlugin/src/visual/layout/Layouter.java
@@ -12,6 +12,8 @@ import structs.ProteinProjection;
 
 public class Layouter {
 
+    private static double R = 30.0;
+
     public static void AllProjectionsLayout() {
 
         Collection<PPINetworkProjection> projections = DataHandle.getProjections().values();
@@ -34,12 +36,29 @@ public class Layouter {
     public static void GroupNodeLayout(GroupNode groupNode, NodeView nodeView, CyNetworkView networkView) {
         Collection<ProteinProjection> proteinProjections = groupNode.getContext().getInsideProteins();
 
+        int i = 0;
+        int count = proteinProjections.size();
+
         for (ProteinProjection proteinProjection : proteinProjections) {
 
             CyNode node = Cytoscape.getCyNode(proteinProjection.getID());
             NodeView proteinView = networkView.getNodeView(node);
+
+            setProteinViewPosition(proteinView, nodeView, i, count);
+            i++;
+        }
+    }
+
+    private static void setProteinViewPosition(NodeView proteinView, NodeView nodeView, int i, int count) {
+        if (count == 1) {
             proteinView.setXPosition(nodeView.getXPosition());
             proteinView.setYPosition(nodeView.getYPosition());
+        } else {
+            double arg = 2 * Math.PI * i / count;
+            double x_offset = R * Math.cos(arg);
+            double y_offset = R * Math.sin(arg);
+            proteinView.setXPosition(nodeView.getXPosition() + x_offset);
+            proteinView.setYPosition(nodeView.getYPosition() + y_offset);
         }
     }
 }
