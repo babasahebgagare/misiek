8aa26337c8faea98a7fc41e7a2acb2366815aaa3
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2008-11-26 22:42:31 +0000

    przeniesienie genaratora colorow

---
+++ MCVPlugin/src/utils/ColorGenerator.java
@@ -0,0 +1,13 @@
+package utils;
+
+import java.awt.Color;
+import java.util.Random;
+
+public class ColorGenerator {
+
+    private static Random rand = new Random(1243242);
+
+    public static Color generateColor(String FamilyName) {
+        return new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
+    }
+}
