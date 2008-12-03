711a5c673da992458d5c7c12dd9833823a5951af
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2008-12-03 12:36:59 +0000

    wyrzucenie mojego paska

--- trunk/MCVPlugin/src/main/MenusHandle.java
+++ trunk/MCVPlugin/src/main/MenusHandle.java
@@ -1,39 +1,10 @@
 package main;
 
-import javax.swing.JProgressBar;
 import javax.swing.JTextArea;
-import javax.swing.SwingUtilities;
 
 public class MenusHandle {
 
-    private static float progress;
     private static JTextArea memo = null;
-    private static JProgressBar progressBar = null;
-
-    public static void setProgress(double p) {
-        progress = (float) p;
-        progressBar.setValue(Math.round(progress));
-    }
-
-    public static void incProgress(double inc) {
-
-        progress += inc;
-        SwingUtilities.invokeLater(new Runnable() {
-
-            public void run() {
-                progressBar.setValue(Math.round(progress));
-            }
-        });
-        System.out.println(progress);
-    }
-
-    public static JProgressBar getProgressBar() {
-        return progressBar;
-    }
-
-    public static void setProgressBar(JProgressBar progressBar) {
-        MenusHandle.progressBar = progressBar;
-    }
 
     public static JTextArea getMemo() {
         return memo;
