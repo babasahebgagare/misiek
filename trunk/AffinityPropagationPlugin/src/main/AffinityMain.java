7884a59072e37392b7d59b3efd29e834342939e0
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2009-01-07 01:23:37 +0000

    Affinity propagation zalozenie projektu

---
+++ trunk/AffinityPropagationPlugin/src/main/AffinityMain.java
@@ -0,0 +1,11 @@
+package main;
+
+import cytoscape.plugin.CytoscapePlugin;
+
+public class AffinityMain extends CytoscapePlugin {
+
+    public AffinityMain() {
+        System.out.println("Affinity propagation");
+    }
+}
+
