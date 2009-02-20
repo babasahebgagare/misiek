668d6724d366ddf33299e272f1d077a4505a25bf
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2009-02-20 15:51:03 +0000

    sortowanie po rozmiarze klastrow

---
+++ trunk/AffinityPropagationPlugin/src/algorithm/smart/Cluster.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithm.smart;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ *
+ * @author misiek
+ */
+public class Cluster<T> implements Comparable {
+
+    private String name;
+    private Collection<T> elements = new HashSet<T>();
+
+    Cluster(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int size() {
+        return elements.size();
+    }
+
+    public void add(T element) {
+        elements.add(element);
+    }
+
+    public Collection<T> getElements() {
+        return elements;
+    }
+
+    public int compareTo(Object c) {
+
+        if (this.size() > ((Cluster) c).size()) {
+            return -1;
+        } else if (this.size() < ((Cluster) c).size()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
