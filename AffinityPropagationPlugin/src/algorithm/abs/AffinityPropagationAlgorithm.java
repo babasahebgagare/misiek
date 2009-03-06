5cbfd03bbfee2a94117976067462d54e3e9e4e84
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2009-03-06 01:57:01 +0000

    przeniesienie abstrakcji

--- AffinityPropagationPlugin/src/algorithm/abs/AffinityPropagationAlgorithm.java
+++ AffinityPropagationPlugin/src/algorithm/abs/AffinityPropagationAlgorithm.java
@@ -1,9 +1,16 @@
 package algorithm.abs;
 
+import java.awt.event.ActionListener;
+
 public abstract class AffinityPropagationAlgorithm<T> extends AbstractClusterAlgorithm<T> {
 
     private double lambda = 0.5;
     private int iterations = 10;
+    protected ActionListener iteractionListener = null;
+
+    public void addIterationListener(ActionListener listener) {
+        this.iteractionListener = listener;
+    }
 
     @Override
     public String getShortName() {
