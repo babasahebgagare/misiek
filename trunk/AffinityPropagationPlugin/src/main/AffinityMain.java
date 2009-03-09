1949bdd4ec510a9ac1732cd8ed11c584b99167f0
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2009-03-09 13:03:13 +0000

    przygotowanie panelu na statystyki

--- trunk/AffinityPropagationPlugin/src/main/AffinityMain.java
+++ trunk/AffinityPropagationPlugin/src/main/AffinityMain.java
@@ -3,9 +3,13 @@ package main;
 import cytoscape.Cytoscape;
 import cytoscape.plugin.CytoscapePlugin;
 import cytoscape.view.cytopanels.CytoPanelImp;
+import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+import java.awt.LayoutManager2;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import panel.AffinityPanelController;
+import panel.AffinityStatsPanel;
 
 public class AffinityMain extends CytoscapePlugin {
 
@@ -14,8 +18,16 @@ public class AffinityMain extends CytoscapePlugin {
         CytoPanelImp leftPanel = (CytoPanelImp) Cytoscape.getDesktop().getCytoPanel(SwingConstants.WEST);
 
         AffinityPanelController pc = new AffinityPanelController();
+        JPanel myAff = new JPanel();
+
+        myAff.setLayout(new FlowLayout());
         JPanel afpanel = pc.createAffinityPanel();
-        leftPanel.add("Affinity", afpanel);
+        JPanel stats = new AffinityStatsPanel();
+        myAff.add(afpanel);
+        myAff.add(stats);
+
+
+        leftPanel.add("Affinity", myAff);
         System.out.println("Affinity propagation");
     }
 }
