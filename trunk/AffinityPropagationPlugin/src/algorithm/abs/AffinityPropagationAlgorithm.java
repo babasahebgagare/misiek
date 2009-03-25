7e3385f7c749e0c63a9eb0cfc8c9422c74a94964
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2009-03-25 22:51:03 +0000

    unification of affinity algorithms

--- trunk/AffinityPropagationPlugin/src/algorithm/abs/AffinityPropagationAlgorithm.java
+++ trunk/AffinityPropagationPlugin/src/algorithm/abs/AffinityPropagationAlgorithm.java
@@ -1,25 +1,31 @@
 package algorithm.abs;
 
+import algorithm.smart.Cluster;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
-public abstract class AffinityPropagationAlgorithm<T> extends AbstractClusterAlgorithm<T> {
+public abstract class AffinityPropagationAlgorithm<String> extends AbstractClusterAlgorithm<String> {
 
     private double lambda;
     private int iterations;
+    protected boolean convergence;
     protected Integer convits = null;
     protected ActionListener iteractionListenerOrNull = null;
+    protected Map<String, Cluster<String>> assigments;
 
     public void addIterationListener(final ActionListener listener) {
         this.iteractionListenerOrNull = listener;
     }
 
     @Override
-    public String getShortName() {
+    public java.lang.String getShortName() {
         return "AP";
     }
 
     @Override
-    public String getName() {
+    public java.lang.String getName() {
         return "Affinity Propagation";
     }
 
@@ -47,5 +53,70 @@ public abstract class AffinityPropagationAlgorithm<T> extends AbstractClusterAlg
         this.convits = convits;
     }
 
+    @Override
+    public Map<String, String> doCluster() {
+        final Map<String, Cluster<String>> help = doClusterAssoc();
+        final Map<String, String> res = new HashMap<String, String>();
+
+        for (Entry<String, Cluster<String>> entry : help.entrySet()) {
+            for (String obj : entry.getValue().getElements()) {
+                res.put(obj, entry.getKey());
+            }
+        }
+
+        return res;
+    }
+
+    public Map<String, Cluster<String>> doClusterAssoc() {
+        int iters = getIterations();
+        if (iteractionListenerOrNull != null) {
+   //         iteractionListenerOrNull.actionPerformed(new ActionEvent(new IterationData(1, examplars.size()), 0, "ITERATION"));
+        }
+
+        for (int iteration = 0; iteration < iters; iteration++) {
+
+            copyResponsibilies();
+            computeResponsibilities();
+            avgResponsibilies();
+
+            copyAvailabilities();
+            computeAvailabilities();
+            avgAvailabilities();
+
+            if (iteration + 1 != iterations && iteractionListenerOrNull != null) {
+                computeCenters();
+      //          iteractionListenerOrNull.actionPerformed(new ActionEvent(new IterationData(iteration + 2, centers.size()), 0, "ITERATION"));
+            }
+            convergence = checkConvergence();
+            if (convergence) {
+                break;
+            }
+        }
+
+
+        computeCenters();
+        computeAssigments();
+
+        return assigments;
+    }
+
     public abstract void setSimilarities(double[][] sim);
+
+    protected abstract void copyResponsibilies();
+
+    protected abstract void computeResponsibilities();
+
+    protected abstract void avgResponsibilies();
+
+    protected abstract void copyAvailabilities();
+
+    protected abstract void computeAvailabilities();
+
+    protected abstract void avgAvailabilities();
+
+    protected abstract void computeCenters();
+
+    protected abstract void computeAssigments();
+
+    protected abstract boolean checkConvergence();
 }
