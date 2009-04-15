46feb3f864742442ee4035f8f78f983cda6f11a9
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2009-04-15 22:24:12 +0000

    findbugs tests

--- AffinityPropagationConsole/src/affinitymain/Main.java
+++ AffinityPropagationConsole/src/affinitymain/Main.java
@@ -8,25 +8,22 @@ public class Main {
 
     /**
      * @param args the command line arguments
+     * @throws Exception
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         if (args.length == 6) {
-            try {
-                String filepath = String.valueOf(args[0]);
-                String outpath = String.valueOf(args[1]);
-                Double lambda = Double.valueOf(args[2]);
-                Integer iterations = Integer.valueOf(args[3]);
-                Double preferences = Double.valueOf(args[4]);
-                String kind = String.valueOf(args[5]);
+            String filepath = String.valueOf(args[0]);
+            String outpath = String.valueOf(args[1]);
+            Double lambda = Double.valueOf(args[2]);
+            Integer iterations = Integer.valueOf(args[3]);
+            Double preferences = Double.valueOf(args[4]);
+            String kind = String.valueOf(args[5]);
 
-                RunAlgorithm alg = new RunAlgorithm(filepath, outpath, lambda, iterations, preferences, kind);
+            RunAlgorithm alg = new RunAlgorithm(filepath, outpath, lambda, iterations, preferences, kind);
 
-                alg.setParemeters();
-                alg.run();
-            } catch (Exception e) {
-                System.out.println(e.getStackTrace()[0]);
-                System.out.println("Error during running: java -jar AffinityPropagation.jar <input> <ouput> <lambda> <iterations> <preferences> <centers or clusters>");
-            }
+            alg.setParemeters();
+            alg.run();
+
         } else {
             System.out.println("Bad args count: java -jar AffinityPropagation.jar <input> <ouput> <lambda> <iterations> <preferences> <centers or clusters>");
         }
