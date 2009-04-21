6d6830d123dce19d2030be125711835f088a8837
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2009-04-21 23:23:00 +0000

    convits added

--- AffinityPropagationConsole/src/affinitymain/Main.java
+++ AffinityPropagationConsole/src/affinitymain/Main.java
@@ -11,21 +11,22 @@ public class Main {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        if (args.length == 6) {
+        if (args.length == 7) {
             String filepath = String.valueOf(args[0]);
             String outpath = String.valueOf(args[1]);
             Double lambda = Double.valueOf(args[2]);
             Integer iterations = Integer.valueOf(args[3]);
-            Double preferences = Double.valueOf(args[4]);
-            String kind = String.valueOf(args[5]);
+            Integer convits = Integer.valueOf(args[4]);
+            Double preferences = Double.valueOf(args[5]);
+            String kind = String.valueOf(args[6]);
 
-            RunAlgorithm alg = new RunAlgorithm(filepath, outpath, lambda, iterations, preferences, kind);
+            RunAlgorithm alg = new RunAlgorithm(filepath, outpath, lambda, iterations, convits, preferences, kind);
 
             alg.setParemeters();
             alg.run();
 
         } else {
-            System.out.println("Bad args count: java -jar AffinityPropagation.jar <input> <ouput> <lambda> <iterations> <preferences> <centers or clusters>");
+            System.out.println("Bad args count: java -jar AffinityPropagation.jar <input> <ouput> <lambda> <iterations> <convits> <preferences> <centers or clusters>");
         }
     }
 }
