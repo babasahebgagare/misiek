45572225c5a7a3cef3cfb4afa44dc8b374eff53a
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2009-05-13 19:49:58 +0000

    line parsing potentially

--- trunk/AffinityPropagationConsole/src/affinitymain/Main.java
+++ trunk/AffinityPropagationConsole/src/affinitymain/Main.java
@@ -1,5 +1,7 @@
 package affinitymain;
 
+import java.util.Map;
+
 /**
  *
  * @author misiek
@@ -19,14 +21,27 @@ public class Main {
             Integer convits = Integer.valueOf(args[4]);
             Double preferences = Double.valueOf(args[5]);
             String kind = String.valueOf(args[6]);
-
             RunAlgorithm alg = new RunAlgorithm(filepath, outpath, lambda, iterations, convits, preferences, kind, false);
 
             alg.setParemeters();
             alg.run();
-
         } else {
             System.out.println("Bad args count: java -jar AffinityPropagation.jar <input> <ouput> <lambda> <iterations> <convits> <preferences> <centers or clusters>");
         }
+        Map<String, String> map = CommandLineParser.parseTokens(args);
+
+        String filepath = map.get("in");
+        String outpath = map.get("out");
+        Double lambda = Double.valueOf(map.get("lam"));
+        Integer iterations = Integer.valueOf(map.get("it"));
+        Integer convits = Integer.valueOf(map.get("con"));
+        Double preferences = Double.valueOf(map.get("p"));
+        String kind = String.valueOf(map.get("kind"));
+
+        RunAlgorithm alg = new RunAlgorithm(filepath, outpath, lambda, iterations, convits, preferences, kind, false);
+
+        alg.setParemeters();
+        alg.run();
+
     }
 }
