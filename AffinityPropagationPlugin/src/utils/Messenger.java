babdaccce306c17b4a1e47155b009845f2da5b1a
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2009-05-13 07:21:45 +0000

    question warning

--- AffinityPropagationPlugin/src/utils/Messenger.java
+++ AffinityPropagationPlugin/src/utils/Messenger.java
@@ -14,4 +14,14 @@ public class Messenger {
         System.out.println(ex.getMessage());
         JOptionPane.showMessageDialog(Cytoscape.getDesktop(), ex.getMessage());
     }
+
+    public static int confirmWarning(Object message) {
+        System.out.println(message);
+        return JOptionPane.showConfirmDialog(Cytoscape.getDesktop(), message, "Warning.", JOptionPane.WARNING_MESSAGE);
+    }
+
+    public static int confirmInfo(Object message) {
+        System.out.println(message);
+        return JOptionPane.showConfirmDialog(Cytoscape.getDesktop(), message, "Info.", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
