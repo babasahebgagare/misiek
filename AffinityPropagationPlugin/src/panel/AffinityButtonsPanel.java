4667d44a3ac00fd44c1d1f754bc7339c4e06e2fe
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2009-05-14 19:46:39 +0000

    affinity help

--- AffinityPropagationPlugin/src/panel/AffinityButtonsPanel.java
+++ AffinityPropagationPlugin/src/panel/AffinityButtonsPanel.java
@@ -6,6 +6,10 @@
  */
 package panel;
 
+import help.AffHelpBroker;
+import java.awt.event.ActionEvent;
+import javax.help.CSH;
+
 /**
  *
  * @author misiek
@@ -33,12 +37,14 @@ public class AffinityButtonsPanel extends javax.swing.JPanel {
 
         refreshButton = new javax.swing.JButton();
         startButton = new javax.swing.JButton();
+        helpButton = new javax.swing.JButton();
 
         setMaximumSize(new java.awt.Dimension(270, 32767));
         setPreferredSize(new java.awt.Dimension(270, 25));
 
         refreshButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/icons/refresh.png"))); // NOI18N
         refreshButton.setText("Refresh");
+        refreshButton.setMargin(new java.awt.Insets(2, 6, 2, 14));
         refreshButton.setName("refreshButton"); // NOI18N
         refreshButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -48,10 +54,19 @@ public class AffinityButtonsPanel extends javax.swing.JPanel {
 
         startButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/icons/run.png"))); // NOI18N
         startButton.setText("Start");
+        startButton.setMargin(new java.awt.Insets(2, 6, 2, 14));
         startButton.setName("startButton"); // NOI18N
         startButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 startButtonActionPerformed(evt);
+            }
+        });
+
+        helpButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/icons/help.png"))); // NOI18N
+        helpButton.setName("helpButton"); // NOI18N
+        helpButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                helpButtonActionPerformed(evt);
             }
         });
 
@@ -61,9 +76,11 @@ public class AffinityButtonsPanel extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(refreshButton, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(refreshButton, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(startButton, javax.swing.GroupLayout.DEFAULT_SIZE, 133, Short.MAX_VALUE)
+                .addComponent(startButton, javax.swing.GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(helpButton, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -71,6 +88,7 @@ public class AffinityButtonsPanel extends javax.swing.JPanel {
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                 .addComponent(refreshButton)
                 .addComponent(startButton))
+            .addComponent(helpButton)
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -82,7 +100,14 @@ public class AffinityButtonsPanel extends javax.swing.JPanel {
 
         pc.doCluster();
 }//GEN-LAST:event_startButtonActionPerformed
+
+    private void helpButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_helpButtonActionPerformed
+        CSH.DisplayHelpFromSource csh = new CSH.DisplayHelpFromSource(AffHelpBroker.getHelpBroker("Introduction"));
+        csh.actionPerformed(new ActionEvent(this, 120, "Introduction"));
+    }//GEN-LAST:event_helpButtonActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton helpButton;
     private javax.swing.JButton refreshButton;
     private javax.swing.JButton startButton;
     // End of variables declaration//GEN-END:variables
