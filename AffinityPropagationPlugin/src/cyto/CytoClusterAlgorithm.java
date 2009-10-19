0e03c0e31c57b77f966aaf5731a1941bfe2ab2d7
mw219725 <mw219725@ab4019dd-f955-0410-9b33-af83f89102aa>
2009-10-19 21:26:01 +0000

    license

--- AffinityPropagationPlugin/src/cyto/CytoClusterAlgorithm.java
+++ AffinityPropagationPlugin/src/cyto/CytoClusterAlgorithm.java
@@ -1,35 +1,35 @@
-/* vim: set ts=2: */
-/**
- * Copyright (c) 2008 The Regents of the University of California.
- * All rights reserved.
+/* ===========================================================
+ * APGraphClusteringPlugin : Java implementation of Affinity Propagation
+ * algorithm as Cytoscape plugin.
+ * ===========================================================
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions, and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above
- *      copyright notice, this list of conditions, and the following
- *      disclaimer in the documentation and/or other materials provided
- *      with the distribution.
- *   3. Redistributions must acknowledge that this software was
- *      originally developed by the UCSF Computer Graphics Laboratory
- *      under support by the NIH National Center for Research Resources,
- *      grant P41-RR01081.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Project Info:  http://bioputer.mimuw.edu.pl/veppin/
+ * Sources: http://code.google.com/p/misiek/
  *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ * APGraphClusteringPlugin  Copyright (C) 2008-2009
+ * Authors:  Michal Wozniak (code) (m.wozniak@mimuw.edu.pl)
+ *           Janusz Dutkowski (idea) (j.dutkowski@mimuw.edu.pl)
+ *           Jerzy Tiuryn (supervisor) (tiuryn@mimuw.edu.pl)
  */
+
+
 package cyto;
 
 import cytoscape.task.TaskMonitor;
--- AffinityPropagationPlugin/src/panel/AffinityButtonsPanel.java
+++ AffinityPropagationPlugin/src/panel/AffinityButtonsPanel.java
@@ -1,19 +1,40 @@
-
-/*
- * JActionButton.java
+/* ===========================================================
+ * APGraphClusteringPlugin : Java implementation of Affinity Propagation
+ * algorithm as Cytoscape plugin.
+ * ===========================================================
  *
- * Created on 2009-05-13, 14:16:11
+ *
+ * Project Info:  http://bioputer.mimuw.edu.pl/veppin/
+ * Sources: http://code.google.com/p/misiek/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ * APGraphClusteringPlugin  Copyright (C) 2008-2009
+ * Authors:  Michal Wozniak (code) (m.wozniak@mimuw.edu.pl)
+ *           Janusz Dutkowski (idea) (j.dutkowski@mimuw.edu.pl)
+ *           Jerzy Tiuryn (supervisor) (tiuryn@mimuw.edu.pl)
  */
+
 package panel;
 
 import help.AffHelpBroker;
 import java.awt.event.ActionEvent;
 import javax.help.CSH;
 
-/**
- *
- * @author misiek
- */
 public class AffinityButtonsPanel extends javax.swing.JPanel {
 
     private AffinityPanelController pc;
